b3dca0230da86131022603be0ad93721558255e2
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2021-12-24 15:59:36 +0100

    Edit contact icons

--- lib/main.dart
+++ lib/main.dart
@@ -21,87 +21,122 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('images/tlo2.jpg'),
-                fit: BoxFit.fill,
-              ),
-            ),
+            // decoration: const BoxDecoration(
+            //   image: DecorationImage(
+            //     image: AssetImage('images/tlo2.jpg'),
+            //     fit: BoxFit.fill,
+            //   ),
+            // ),
             child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/tlo'),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Łukasz Walencik',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Junior Flutter Developer',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Social Media',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  CircleAvatar(
-                    radius: 100,
-                    backgroundImage: AssetImage('images/tlo'),
+                  GFIconButton(
+                    onPressed: () {},
+                    icon: Icon(FontAwesomeIcons.twitter),
                   ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Łukasz Walencik',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Junior Flutter Developer',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Social Media',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      GFIconButton(
-                        onPressed: () {},
-                        icon: Icon(FontAwesomeIcons.twitter),
-                      ),
-                      GFIconButton(
-                          onPressed: () {},
-                          icon: Icon(FontAwesomeIcons.facebook,
-                              color: Colors.white, size: 25.0)),
-                      GFIconButton(
-                        onPressed: () {},
-                        icon: Icon(FontAwesomeIcons.linkedin),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Contact',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      GFIconButton(
-                        onPressed: () {},
-                        icon: Icon(FontAwesomeIcons.at),
-                      ),
-                      GFIconButton(
-                        onPressed: () {},
-                        icon: Icon(FontAwesomeIcons.phone),
-                      ),
-                      GFIconButton(
-                        onPressed: () {},
-                        icon: Icon(FontAwesomeIcons.sms),
-                      ),
-                      GFIconButton(
-                        onPressed: () {},
-                        icon: Icon(FontAwesomeIcons.whatsapp),
-                      ),
-                      GFIconButton(
-                        onPressed: () {},
-                        icon: Icon(FontAwesomeIcons.facebookMessenger),
-                      ),
-                    ],
+                  GFIconButton(
+                      onPressed: () {},
+                      icon: Icon(FontAwesomeIcons.facebook,
+                          color: Colors.white, size: 25.0)),
+                  GFIconButton(
+                    onPressed: () {},
+                    icon: Icon(FontAwesomeIcons.linkedin),
                   ),
                 ],
               ),
-            )),
+              SizedBox(height: 20),
+              Text(
+                'Contact',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.green,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      icon: Icon(FontAwesomeIcons.phone),
+                    ),
+                  ),
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.blue,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      icon: Icon(FontAwesomeIcons.sms),
+                    ),
+                  ),
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.red,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      icon: Icon(FontAwesomeIcons.at),
+                    ),
+                  ),
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.blue,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      icon: Icon(FontAwesomeIcons.facebookMessenger),
+                    ),
+                  ),
+                  Ink(
+                    decoration: const ShapeDecoration(
+                      color: Colors.green,
+                      shape: CircleBorder(),
+                    ),
+                    child: IconButton(
+                      icon: const Icon(FontAwesomeIcons.whatsapp),
+                      color: Colors.white,
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )),
       ),
     );
   }
